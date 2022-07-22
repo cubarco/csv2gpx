3a9dd9947a2a8198699c25da10886c603a085659
cubarco <marco.l.here@gmail.com>
2022-07-22 11:36:17 +0800

    Don't use jsdelivr anymore.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://cdn.jsdelivr.net/gh/cubarco/csv2gpx@gh-pages/'
+    ? '/'
+//    ? 'https://cdn.jsdelivr.net/gh/cubarco/csv2gpx@gh-pages/'
     : '/'
 }
